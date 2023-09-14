a91874de6a574ffc45fa65e7b3491e159d3d5d59
AlexsandroRyan <alexsandrocosta855@gmail.com>
2023-09-13 21:32:56 -0300

    remove wrong folder

--- src/aula1/core.clj
+++
@@ -1,9 +0,0 @@
-(ns aula1.core
-  (:require [datomic.client.api :as d]))
-
-(def client (d/client {:server-type :datomic-local
-                       :system "dev"}))
-
-(d/create-database client {:db-name "aula1"})
-
-(d/list-databases client {})
